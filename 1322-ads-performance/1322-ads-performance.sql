eaf3966de74a64166ac7e98fe8e90efdfb74379c
Rohith Kumar Diwakar <95113910+Rvernekar854@users.noreply.github.com>
2022-01-29 17:28:42 -0500

    Time: 638 ms (25.47%), Space: 0B (100.00%) - LeetHub

--- 1322-ads-performance/1322-ads-performance.sql
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-select ad_id, round(coalesce(sum(if(action = "clicked",1,0))/sum(if(action = "viewed",1,0)+if(action = "clicked",1,0)),0)*100,2) as ctr
-from ads
-group by ad_id
-order by ctr desc, ad_id asc
-
+select ad_id, round(coalesce(sum(if(action ="Clicked",1,0))/sum(if(action ="Viewed",1,0)+if(action ="Clicked",1,0)),0)*100,2) as ctr
+from Ads
+group by 1
+order by ctr desc,ad_id 
