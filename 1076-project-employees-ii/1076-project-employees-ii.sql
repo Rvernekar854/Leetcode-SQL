1443876fa033aa254ee67fc0b4ce25c5b0cb3730
Rohith Kumar Diwakar <95113910+Rvernekar854@users.noreply.github.com>
2022-01-31 12:51:18 -0500

    Time: 4377 ms (46.80%), Space: 0B (100.00%) - LeetHub

--- 1076-project-employees-ii/1076-project-employees-ii.sql
+++ 1076-project-employees-ii/1076-project-employees-ii.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 with HighstPPL as
-(select top 1 with ties project_id, count(employee_id) as MAXppl
+(select top 1 with ties project_id, count(project_id) as MAXppl
       from  Project 
        group by project_id
         order by  MAXppl desc)
