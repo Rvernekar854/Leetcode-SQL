3faa683409b9cc20b9d76ccc19705eb1d7e9a61b
Rohith Kumar Diwakar <95113910+Rvernekar854@users.noreply.github.com>
2022-05-04 19:01:53 -0400

    Time: 614 ms (75.32%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
 
-with que_perct as
-(Select round(count(if(rating < 3,1,null))*100/count(*),2) as poor_query_percentage from Queries group by query_name)
 
 select query_name, round(avg(rating/position),2) as quality, round(count(if(rating < 3,1,null))*100/count(*),2) as poor_query_percentage
 from Queries
