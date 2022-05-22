fdcbe6cf2a11ce20899e78737c71fa70a9deee61
Rohith Kumar Diwakar <95113910+Rvernekar854@users.noreply.github.com>
2022-05-22 17:34:14 -0400

    Time: 563 ms (42.67%), Space: 0B (100.00%) - LeetHub

--- 1853-convert-date-format/1853-convert-date-format.sql
+++ 1853-convert-date-format/1853-convert-date-format.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
-with l1 as (Select dayname(day) as day,day(day) as Dates, monthname(day) as month, year(day)as years from Days)
+#with l1 as (Select dayname(day) as day,day(day) as Dates, monthname(day) as month, year(day)as years from Days)
 
-Select concat(day,", ",month," ",Dates,", ",years) as day from l1
+#Select concat(day,", ",month," ",Dates,", ",years) as day from l1
+
+SELECT DATE_FORMAT(day, "%W, %M %e, %Y") AS day 
+FROM Days
